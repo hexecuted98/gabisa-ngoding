1d29948e32e8e9fa7601d74832f3511c2b05d9e8
AdjiDev <creeperindoors@gmail.com>
2025-10-31 20:36:54 +0700

    release: gabut

--- src/helper/errors.ts
+++ src/helper/errors.ts
@@ -21,18 +21,25 @@ export default function atasiError(e: GabisaNgoding_err): never | void {
             return; 
         case GabisaNgoding_err.GBS_NGODING_ERROR_MALLOC:
             console.log("{biru}GabisaNgoding{reset}: gagal alokasi memori (malloc error).".replace("{reset}", reset).replace("{biru}", biru));
+            break;
         case GabisaNgoding_err.GBS_NGODING_ERROR_IO:
             console.log("{biru}GabisaNgoding{reset}: kesalahan input/output (IO error).".replace("{reset}", reset).replace("{biru}", biru));
+            break;
         case GabisaNgoding_err.GBS_NGODING_ERROR_INVALID_TARGET:
             console.log("{biru}GabisaNgoding{reset}: pointer target tidak valid.".replace("{reset}", reset).replace("{biru}", biru));
+            break;
         case GabisaNgoding_err.GBS_NGODING_ERROR_UNEXPECTED_LOOP_END:
             console.log("{biru}GabisaNgoding{reset}: ditemukan 'sepuh' tanpa 'puh' (loop end tak terduga).".replace("{reset}", reset).replace("{biru}", biru));
+            break;
         case GabisaNgoding_err.GBS_NGODING_ERROR_UNCLOSED:
             console.log("{biru}GabisaNgoding{reset}: loop 'puh' tidak pernah ditutup (unclosed loop).".replace("{reset}", reset).replace("{biru}", biru));
+            break;
         case GabisaNgoding_err.GBS_NGODING_OUT_OF_BOUNDS:
             console.log("{biru}GabisaNgoding{reset}: pointer keluar dari batas memori.".replace("{reset}", reset).replace("{biru}", biru));
+            break;
         case GabisaNgoding_err.GBS_NGODING_END_OF_FILE:
             console.log("{biru}GabisaNgoding{reset}: mencapai akhir file (EOF).".replace("{reset}", reset).replace("{biru}", biru));
+            break;
         default:
             console.log("{biru}GabisaNgoding{reset}: error code 0 [UNKNOWN].".replace("{reset}", reset).replace("{biru}", biru));
     }
